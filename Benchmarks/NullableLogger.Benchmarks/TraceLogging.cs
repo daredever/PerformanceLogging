5428611c8a768faa926a8a68208eefa0e88ba62a
Roman Shcherbakov <daredever@gmail.com>
2020-06-12 10:00:15 +0300

    add benchmark

---
+++ Benchmarks/NullableLogger.Benchmarks/TraceLogging.cs
@@ -0,0 +1,76 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace NullableLogger.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class TraceLogging
+    {
+        private readonly ILogger<TraceLogging> _logger;
+        private readonly INullableLogger _wrappedLogger;
+        private const int Length = 100;
+
+        public TraceLogging()
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .SetMinimumLevel(LogLevel.Debug)
+                    .AddConsole(options => options.IncludeScopes = true);
+            });
+
+            // Create logger.
+            _logger = loggerFactory.CreateLogger<TraceLogging>();
+            _wrappedLogger = _logger.Wrap();
+        }
+
+        [Benchmark]
+        public void ClassicWayLog()
+        {
+            for (var i = 0; i < Length; i++)
+            {
+                _logger.LogTrace($"Log line #{i}.");
+            }
+        }
+
+        [Benchmark]
+        public void ClassicWayLogWithCheck()
+        {
+            for (var i = 0; i < Length; i++)
+            {
+                if (_logger.IsEnabled(LogLevel.Trace))
+                {
+                    _logger.LogTrace($"Log line #{i}.");
+                }
+            }
+        }
+
+        [Benchmark]
+        public void NewWayLogExtension()
+        {
+            for (var i = 0; i < Length; i++)
+            {
+                _logger.Trace()?.Log($"Log line #{i}.");
+            }
+        }
+
+        [Benchmark]
+        public void NewWayLogWrap()
+        {
+            var wrappedLogger = _logger.Wrap();
+            for (var i = 0; i < Length; i++)
+            {
+                wrappedLogger.Trace?.Log($"Log line #{i}.");
+            }
+        }
+
+        [Benchmark]
+        public void NewWayLogWrapOnce()
+        {
+            for (var i = 0; i < Length; i++)
+            {
+                _wrappedLogger.Trace?.Log($"Log line #{i}.");
+            }
+        }
+    }
+}
